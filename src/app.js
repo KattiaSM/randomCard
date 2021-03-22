0307f302a5a5492e4d9c3dab118e655b096f0539
Kattia Salazar Mesen <kattiasm@gmail.com>
2021-03-22 16:53:49 +0000

    Entregable Random Card

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  var numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  var shape = ["Diamonds", "Spades", "Hearts", "Clubs"];
+  var randomCardNumber = Math.floor(Math.random() * numbers.length);
+  var randomShapeNumber = Math.floor(Math.random() * shape.length);
+  var choseShape = shape[randomShapeNumber];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //agregar el content a la card
+  document.getElementById("cardContent").innerHTML = numbers[randomCardNumber];
+  document.getElementById("theCard").classList.add("card");
+  document.getElementById("theCard").classList.add("getshape"(choseShape));
+};
+
+//retorna el numero y shape aleatorio
+getshape => {
+  switch (getshape) {
+    case "Diamonds":
+      return "shape-diamonds";
+    case "Spades":
+      return "shape-spades";
+    case "Hearts":
+      return "shape-hearts";
+    case "Clubs":
+      return "shape-clubs";
+  }
 };
